885c703dc03fa3e8bf7fe2ef7142749983c4779b
Claudia A Rodriguez V <claudiaarv@yahoo.com>
2023-02-12 20:12:15 -0600

    carga inicial

---
+++ src/test/java/com/project/discovery/DiscovveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
